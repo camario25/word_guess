baf72788a22ae05762afc307c0455a4a319f87f8
camario25 <camario25@yahoo.com>
2018-11-27 11:57:55 -0800

    able to get words list filter by level

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+var wordApi = "http://app.linkedin-reach.io/words";
+var levels = {
+  1: '01',
+  2: '02',
+  3: '03',
+  4: '04',
+  5: '05',
+  6: '06',
+  7: '07',
+  8: '08',
+  9: '09',
+  10: '10'
+}
 $(document).ready(function() {
-  console.log('JS is connected');
+  levelDropdown();
 });
+
+function levelDropdown() {
+  for (var level in levels) {
+    $("#level").append('<option value="' + level + '">' + levels[level] + '</option>');
+  }
+}
+
+  
+$('form').on('submit', function(e) {
+  e.preventDefault();
+  var level = parseInt($('#level').val());
+
+  $.ajax({
+    method: 'GET',
+    url: wordApi,
+    data: {
+      difficulty: level,
+      count: 50
+    },
+    success: wordsFetched,
+    error: wordError
+  });
+});
+
+function wordsFetched (response) {
+  console.log(response);
+}
+
+function wordError (error) {
+  console.log(error);
+}
