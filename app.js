0269402107dbe129e5b85cfcc09d138d4da728d9
camario25 <camario25@yahoo.com>
2018-12-02 10:30:17 -0800

    found font, color scheme and styled table

--- app.js
+++ app.js
@@ -63,24 +63,8 @@ $('form').on('submit', function(e) {
 });
 
 function wordsFetched (response) {
+  gameStart();
   var checkWordArr = [];
-  var score = 0;
-  $('#level').prop('disabled', true);
-  $('#guessWord').empty();
-  $('#counter').empty();
-  $('#alphabet').empty();
-  $('#gameResult').empty();
-  $('#face').html('<img src="images/head.png" alt="head">');
-  $('#entireWord').html('<p>You can guess the whole word here:  <input type="text" id="fullWord" title="Only English Letters Allowed"><button id="submitFullWord" disabled>submit</button>')
-  alphabet.forEach(function (el) {
-    $('#alphabet').append('<button class="btn" value="' + el + '">' + el + '</button>')
-  });
-  $('#counterDiv').html('<p>Incorrect Guesses Remaining:  <span id="counter"></span></p>');
-  $('#counter').append(6);
-  $('#usedLettersDiv').html('<p>Incorrect Letters:  <span id="usedLetter"></span></p>');
-  $('#scoreButtons').html('<button id="saveScore">Save Your High Score</button><button id="clearLeaders">Clear High Scores</button>');
-
-  
   var wordsArr = response.split('\n');
   var wordSelect = wordsArr[randomInt(101)];
   var wordSplitArr = wordSelect.split('');
@@ -98,14 +82,7 @@ function wordsFetched (response) {
   $('#submitFullWord').on('click', function () {
     var fullWordValue = $('#fullWord').val().toLowerCase();
     if (fullWordValue === wordSelect){
-      console.log(fullWordValue, wordSelect);
-      $('#face').html('<img src="images/win.png">');
-      $('#gameResult').html('<h2>You are the winner!</h2>');
-      score += (parseInt($('#level').val()) * 2);
-      var totalScore = score + (parseInt(sessionStorage.getItem('playerScore')));
-      sessionStorage.setItem('playerScore', totalScore);
-      console.log(score, totalScore);
-      $('#currentScore').html('Current Score for ' + sessionStorage.getItem('playerName') + ' is ' + sessionStorage.getItem('playerScore'));
+      win();
       disableGuesses();
     } else {
       countDown();
@@ -118,21 +95,14 @@ function wordsFetched (response) {
   });
   $('.btn').on('click', function() {
     var letter = $(this).val().toLowerCase();
-    console.log(letter);
     $(this).prop('disabled', true);
     if (wordSplitArr.includes(letter)) {
       wordSplitArr.forEach(function (el, i) {
         if (el === letter) {
           checkWordArr[i] = letter;
-          console.log('yes', el, checkWordArr);
           $('#guessWord :nth-child(' + (i+1) + ')').replaceWith('<span>' + el + '</span>');
           if (checkWordArr.join('') === wordSelect) {
-            $('#face').html('<img src="images/win.png">');
-            $('#gameResult').html('<h2>You are the winner!</h2>');
-            score += (parseInt($('#level').val()) * 2);
-            var totalScore = score + (parseInt(sessionStorage.getItem('playerScore')));
-            console.log(score, totalScore);
-            sessionStorage.setItem('playerScore', totalScore);
+            win();
             disableGuesses();
           }
         } 
@@ -148,10 +118,11 @@ function wordsFetched (response) {
     }
   })
   $('#saveScore').on('click', function() {
-    player = sessionStorage.getItem('playerName');
-    score = sessionStorage.getItem('playerScore');
-    if (parseInt(score) > parseInt(window.localStorage.getItem(player))) {
-    window.localStorage.setItem(player, score);
+    var player = sessionStorage.getItem('playerName');
+    var sessionScore = sessionStorage.getItem('playerScore');
+    var localScore = window.localStorage.getItem(player);
+    if (parseInt(sessionScore) > parseInt(localScore) || localScore === null) {
+    window.localStorage.setItem(player, sessionScore);
   } else {
     $('#alert').html('<h2>You already have a higher score saved.</h2>');
   }
@@ -160,12 +131,30 @@ function wordsFetched (response) {
   
   $('#clearLeaders').on('click', function() {
     localStorage.clear();
+    leaderBoard();
   });
 }
 
+function wordError (error) {
+  $('#alert').html(error);
+}
 
-
-
+function gameStart() {
+  $('#level').prop('disabled', true);
+  $('#guessWord').empty();
+  $('#counter').empty();
+  $('#alphabet').empty();
+  $('#gameResult').empty();
+  $('#face').html('<img src="images/head.png" alt="head">');
+  $('#entireWord').html('<p>You can guess the whole word here:  <input type="text" id="fullWord" title="Only English Letters Allowed"><button id="submitFullWord" disabled>submit</button>')
+  alphabet.forEach(function (el) {
+    $('#alphabet').append('<button class="btn" value="' + el + '">' + el + '</button>')
+  });
+  $('#counterDiv').html('<p>Incorrect Guesses Remaining:  <span id="counter"></span></p>');
+  $('#counter').append(6);
+  $('#usedLettersDiv').html('<p>Incorrect Letters:  <span id="usedLetter"></span></p>');
+  $('#scoreButtons').html('<button id="saveScore">Save Your High Score</button><button id="clearLeaders">Clear High Scores</button>');
+}
 
 function enableFullSubmitButton() {
   if ($('#fullWord').val() !== '') {
@@ -190,13 +179,20 @@ function setPlayer() {
   }
 }
 
+function win() {
+  var score = 0
+  score += (parseInt($('#level').val()) * 2);
+  var totalScore = score + (parseInt(sessionStorage.getItem('playerScore')));
+  sessionStorage.setItem('playerScore', totalScore);
+  $('#face').html('<img src="images/win.png">');
+  $('#gameResult').html('<h2>You are the winner!</h2>');
+  $('#currentScore').html('Current Score for ' + sessionStorage.getItem('playerName') + ' is ' + sessionStorage.getItem('playerScore'));
+}
+
 function leaderBoard() {
   var tempArr = [];
   var leadersNames = Object.keys(localStorage);
   var leadersScores = Object.values(localStorage);
-  
-  console.log(leadersNames, leadersScores);
-  
   for (var player in localStorage) {
     if (parseInt(localStorage[player]) && player !== 'length') {
       tempArr.push([player, parseInt(localStorage[player])]);
@@ -205,16 +201,11 @@ function leaderBoard() {
   var sortedArr = tempArr.sort(function(a, b) {
     return b[1] - a[1];
   });  
-  console.log(sortedArr);
-    $('#leaderBoard').html('<table><tr><th>Player</th><th>High Score</th></tr></table>')
+    $('#leaderBoard').html('<table><caption>Player High Scores</caption><thead><tr><th>Name</th><th>Points</th></tr></thead><tbody></tbody></table>')
   sortedArr.forEach(function (el) {
-    $('table').append('<tr><td>' + el[0] + '</td><td>' + el[1] + '</td></tr>')
+    $('tbody').append('<tr><td>' + el[0] + '</td><td>' + el[1] + '</td></tr>')
   })
   
-}
-
-function wordError (error) {
-  console.log(error);
 }
 
 
@@ -230,6 +221,7 @@ function imageSelect() {
 
 function disableGuesses() {
   $('.btn').prop('disabled', true);
+  $('#fullWord').prop('disabled', true);
   $('#submitFullWord').prop('disabled', true);
   $('#level').prop('disabled', false);
 }
